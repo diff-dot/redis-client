c61b981d1615b62166e190f2f6d84fb7833dc0d1
w <9843576wW!>
2020-02-07 18:32:37 +0900

    first commit

---
+++ src/RedisClient.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+import IORedis from 'ioredis';
+import { ConfigManager, RedisConfig } from '@diff./config-manager';
+
+const retryStrategy = function(times: number) {
+  return Math.min(times * 300, 10000);
+};
+
+export class RedisClient {
+  private static readonly hostClientMap: Map<string, IORedis.Redis> = new Map();
+  private static readonly custerClientMap: Map<string, IORedis.Cluster> = new Map();
+
+  public static host(config: ConfigManager<RedisConfig>, host: string): IORedis.Redis {
+    let client = this.hostClientMap.get(host);
+    if (client) return client;
+
+    const connInfo = config.data.redis.hosts ? config.data.redis.hosts[host] : undefined;
+    if (!connInfo) throw new Error(`undefined redis host : ${host}`);
+
+    client = new IORedis({
+      host: connInfo.host,
+      port: connInfo.port,
+      retryStrategy: retryStrategy,
+      keyPrefix: config.data.redis.keyPrefix
+    });
+
+    this.hostClientMap.set(host, client);
+    return client;
+  }
+
+  public static cluster(config: ConfigManager<RedisConfig>, host: string): IORedis.Cluster {
+    let client = this.custerClientMap.get(host);
+    if (client) return client;
+
+    const connInfo = config.data.redis.clusters ? config.data.redis.clusters[host] : undefined;
+    if (!connInfo) throw new Error(`undefined redis host : ${host}`);
+
+    client = new IORedis.Cluster(connInfo.nodes, {
+      scaleReads: connInfo.scaleReads,
+      clusterRetryStrategy: retryStrategy,
+      redisOptions: {
+        keyPrefix: config.data.redis.keyPrefix
+      }
+    });
+
+    this.custerClientMap.set(host, client);
+    return client;
+  }
+}
