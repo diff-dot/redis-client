eabb4f60d630115f6c9684423dc61750c620bdff
Hyogeun Wang <w@endless9.com>
2020-03-04 17:39:23 +0900

    설정 별도 지정

--- src/RedisClient.ts
+++ src/RedisClient.ts
@@ -10,37 +10,46 @@ const retryStrategy = function(times: number) {
 export class RedisClient {
   private static readonly hostClientMap: Map<string, IORedis.Redis> = new Map();
   private static readonly custerClientMap: Map<string, IORedis.Cluster> = new Map();
+  private static config?: ConfigManager<RedisConfig>;
 
-  public static host(host: string, config: ConfigManager<RedisConfig>): IORedis.Redis {
+  public static setConfig(config: ConfigManager<RedisConfig>) {
+    this.config = config;
+  }
+
+  public static host(host: string): IORedis.Redis {
+    if (!this.config) throw Error('No preference has been specified.');
+
     let client = this.hostClientMap.get(host);
     if (client) return client;
 
-    const connInfo = config.data.redis.hosts ? config.data.redis.hosts[host] : undefined;
+    const connInfo = this.config.data.redis.hosts ? this.config.data.redis.hosts[host] : undefined;
     if (!connInfo) throw new Error(`undefined redis host : ${host}`);
 
     client = new IORedis({
       host: connInfo.host,
       port: connInfo.port,
       retryStrategy: retryStrategy,
-      keyPrefix: config.data.redis.keyPrefix
+      keyPrefix: this.config.data.redis.keyPrefix
     });
 
     this.hostClientMap.set(host, client);
     return client;
   }
 
-  public static cluster(host: string, config: ConfigManager<RedisConfig>): IORedis.Cluster {
+  public static cluster(host: string): IORedis.Cluster {
+    if (!this.config) throw Error('No preference has been specified.');
+
     let client = this.custerClientMap.get(host);
     if (client) return client;
 
-    const connInfo = config.data.redis.clusters ? config.data.redis.clusters[host] : undefined;
+    const connInfo = this.config.data.redis.clusters ? this.config.data.redis.clusters[host] : undefined;
     if (!connInfo) throw new Error(`undefined redis host : ${host}`);
 
     client = new IORedis.Cluster(connInfo.nodes, {
       scaleReads: connInfo.scaleReads,
       clusterRetryStrategy: retryStrategy,
       redisOptions: {
-        keyPrefix: config.data.redis.keyPrefix
+        keyPrefix: this.config.data.redis.keyPrefix
       }
     });
 
