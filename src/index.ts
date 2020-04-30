64ae37f256fee5d6be265941f40f8ca2e7316718
Hyogeun Wang <w@endless9.com>
2020-05-01 00:33:46 +0900

    Redis export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { RedisHostOptions } from './option/RedisHostOptions';
 export { RedisClusterOptions } from './option/RedisClusterOptions';
 export { PipelineCmd } from './PipelineCmd';
 export { ClusterSafePipeline } from './ClusterSafePipeline';
+export { Redis } from 'ioredis';
